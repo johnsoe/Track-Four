8d71a128901dfeadbe7ca25bbb4efa1821213b8b
Evan Johnson <evan.johnson.r@gmail.com>
2024-01-31 23:54:13 -0800

    fix swap spawner for now

--- scenes/manager/swap/swap_spawn_manager.gd
+++ scenes/manager/swap/swap_spawn_manager.gd
@@ -6,17 +6,14 @@ extends Node
 @export var swap_scene: PackedScene
 @export var camera_manager: CameraManager
 
-var camera_position_offset: Vector2
 var required_swap_count: int
 var next_swaps: Dictionary
 
 func _ready():
-	camera_position_offset = camera_manager.calculate_camera_offset()
 	distance_manager.on_distance_updated.connect(on_distance_updated)
 
 
 func on_distance_updated(distance: int):
-	return 
 	
 	if swap_thresholds.any(func(number): return distance == number):
 		# determine which tracks to swap, then spawn swap scene.
@@ -27,18 +24,17 @@ func on_distance_updated(distance: int):
 		required_swap_count = 2
 		next_swaps = {}
 		
+		#TODO: Dynamically size portals based on tracks
 		var portal = swap_scene.instantiate() as Portal
-		var x = (track_manager.get_track_spawn_position(0, 1, TrackManager.LOCATION.TOP)).x - 19
-		var y = (camera_manager.camera_position() - camera_position_offset - Vector2(0, 200)).y
-		portal.global_position = Vector2(x, y)
+		portal.global_position = track_manager.get_track_spawn_position(0, 1, TrackManager.LOCATION.TOP)
 		get_parent().add_child(portal)
+		portal.scale.x = 3
 		portal.set_swap(Vector2(0, 1))
 		portal.on_player_collide.connect(register_swap)
 		
 		portal = swap_scene.instantiate() as Portal
-		x = (track_manager.get_track_spawn_position(0, 1, TrackManager.LOCATION.TOP)).x - 29
-		y = (camera_manager.camera_position() - camera_position_offset - Vector2(0, 200)).y
-		portal.global_position = Vector2(x, y)
+		portal.global_position = track_manager.get_track_spawn_position(1, 1, TrackManager.LOCATION.TOP)
+		portal.scale.x = 3
 		get_parent().add_child(portal)
 		portal.set_swap(Vector2(1, 0))
 		portal.on_player_collide.connect(register_swap)
--- scenes/manager/track_manager.gd
+++ scenes/manager/track_manager.gd
@@ -4,7 +4,8 @@ class_name TrackManager
 enum LOCATION { TOP, BOTTOM }
 
 @export var track_model: TrackModel
-@export var ball_spawn_height_percent: float
+@export var top_spawn_offset_percent: float
+@export var bottom_spawn_offset_percent: float
 @export var camera_manager: CameraManager
 
 @onready var tilemap: TrackTileMap = $TileMap
@@ -25,7 +26,7 @@ func _ready():
 	print(mid_track_position)
 
 func get_track_spawn_position(track: int, level: int, spawn_location: LOCATION):
-	if track < 0 || track > mid_track_position.size():
+	if track < 0 || track > mid_track_position.size() || level < 1 || level > 3:
 		return null
 		
 	match spawn_location:
@@ -36,22 +37,27 @@ func get_track_spawn_position(track: int, level: int, spawn_location: LOCATION):
 
 
 func get_top_spawn_y():
-	camera_manager.get_camera_rect().position.y
+	var top_edge = camera_manager.camera_position() - camera_manager.calculate_camera_offset()
+	var height = camera_manager.calculate_camera_offset().y * 2
+	var top_spawn_offset = height * (top_spawn_offset_percent / 100.0)
+	return top_edge.y - top_spawn_offset
 	
 
 func get_bottom_spawn_y():
 	var bottom_edge = camera_manager.camera_position() + camera_manager.calculate_camera_offset()
 	var height = camera_manager.calculate_camera_offset().y * 2
-	var bottom_spawn_offset = height * (ball_spawn_height_percent / 100.0)
+	var bottom_spawn_offset = height * (bottom_spawn_offset_percent / 100.0)
 	return bottom_edge.y - bottom_spawn_offset
 
 
 func calculate_track_positions_by_level(level: int):
+	if level < 1 || level > 3:
+		return
 	var cell_size = tilemap.cell_quadrant_size
 	var result = mid_track_position[level]
 	match level:
 		1: 
-			result = [track_model.edge_buffer + (track_model.width_level_1 / 2.0), track_model.edge_buffer + track_model.width_level_1 + 1]
+			result = [track_model.edge_buffer + (track_model.width_level_1 / 2.0), track_model.edge_buffer + (track_model.width_level_1 * 1.5) + 1]
 		_:
 			var w = track_model.width_level_2
 			for i in range(0, level + 1):
@@ -59,6 +65,18 @@ func calculate_track_positions_by_level(level: int):
 	
 	mid_track_position[level] = result.map(func (num): return num * cell_size)
 	
+
+func get_track_width(level: int):
+	if level < 1 || level > 3:
+		return
+		
+	var base_width = 0.0
+	match level:
+		1: base_width = track_model.width_level_1
+		2: base_width = track_model.width_level_2
+		3: base_width = track_model.width_level_3
+	
+	return base_width * tilemap.cell_quadrant_size
 
 func handle_level_update(level: int):
 	pass
