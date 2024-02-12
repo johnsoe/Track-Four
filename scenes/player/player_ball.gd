f4a8f7435c85053603bb843457fdcc5c8b01909d
Evan Johnson <evan.johnson.r@gmail.com>
2024-02-12 14:13:25 -0800

    Add a trail, but disable for now

--- scenes/player/player_ball.gd
+++ scenes/player/player_ball.gd
@@ -5,16 +5,19 @@ class_name PlayerBall
 @export var x_speed: int
 @export var sprite_override: CompressedTexture2D
 
+@onready var trail: GPUParticles2D = $Trail
 @onready var sprite = $Sprite2D
 
 func _ready():
 	Events.on_track_clicked.connect(handle_track_click)
 	sprite.texture = sprite_override
+	trail.texture = sprite_override
 
 
 func _process(delta):
 	velocity = Vector2(x_speed, 0)
 	move_and_slide()
+	#trail.process_material.direction = Vector3(velocity.x, velocity.x, 0)
 
 
 func handle_track_click(track: int):
